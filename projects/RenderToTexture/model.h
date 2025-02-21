9ab6f202d1361149301b062101d5ecbd5638b08c
LeftCircle <rcato60@gmail.com>
2025-02-20 20:33:55 -0500

    model view controller format

--- projects/RenderToTexture/model.h
+++ projects/RenderToTexture/model.h
@@ -1,6 +1,12 @@
 #ifndef MODEL_H
 #define MODEL_H
 
+#include "cyCodeBase/cyVector.h"
+#include "cyCodeBase/cyMatrix.h"
+#include "rcCodeBase/rcObjModifier.h"
+
+#include <vector>
+
 // Plane vertices (position and texture coordinates)
 const float planeVertices[] = {
     // positions        // texture coords
@@ -14,5 +20,15 @@ const unsigned int planeIndices[] = {
     0, 1, 2,
     0, 2, 3
 };
+
+class Model{
+private:
+	std::vector<rc::rcTriMeshForGL> meshes;
+
+public:
+	Model();
+	bool load_model(std::string filename);
+
+}
 
 #endif
